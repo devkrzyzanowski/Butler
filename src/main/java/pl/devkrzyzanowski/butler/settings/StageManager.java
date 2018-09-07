c037850188aa548eb64e3ed5379cef419813b1fd
Michał Krzyżanowski <Miisza95@gmail.com>
2018-09-07 15:19:11 +0200

    repair bundles + debug StageManager

--- src/main/java/pl/devkrzyzanowski/butler/settings/StageManager.java
+++ src/main/java/pl/devkrzyzanowski/butler/settings/StageManager.java
@@ -5,6 +5,7 @@
  */
 package pl.devkrzyzanowski.butler.settings;
 
+import java.io.IOException;
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
@@ -27,12 +28,12 @@ public class StageManager {
      *
      */
     public StageManager() {
-//        try {
+        try {
         bundle = ResourceBundle.getBundle("bundles.messages", Locale.getDefault());
-//        } catch (MissingResourceException e) {
-//            bundle = null;
-//            System.err.println(e);
-//        }
+        } catch (MissingResourceException e) {
+            bundle = null;
+            System.err.println(e);
+        }
     }
     
     /**
@@ -40,23 +41,43 @@ public class StageManager {
      * @param stage
      * @param fxml
      */
-    public void newStage(Stage stage, String fxml) {
-        Scene scene = new Scene(getParent(fxml));
-        stage.setScene(scene);
-        stage.setTitle(bundle.getString("error.undefined"));
-        stage.show();
+    public boolean newStage(Stage stage, String fxml) {
+        if (stage != null) {
+            System.err.println("stage is null!");
+            return false;
+        } else if (fxml != null) {
+            System.err.println("fxml is null!");
+            return false;
+        } else {
+            Scene scene = new Scene(getParent(fxml));
+            stage.setScene(scene);
+            stage.setTitle(bundle.getString("error.undefined"));
+            stage.show();
+            return true;
+        }
     }
     
     /**
      *
      * @param stage
      * @param fxml
+     * @return true on success, false on fail
      */
-    public void changeStage(Stage stage, String fxml) {
-        Scene scene = new Scene(getParent(fxml), stage.getScene().getWidth(), stage.getScene().getHeight());
-        stage.setScene(scene);
-        stage.setTitle(bundle.getString("error.undefined"));
-        stage.show();
+    public boolean changeStage(Stage stage, String fxml) {
+        if (stage != null) {
+            System.err.println("stage is null!");
+            return false;
+        } else if (fxml != null) {
+            System.err.println("fxml is null!");
+            return false;
+        } else {
+            Scene scene = new Scene(getParent(fxml), stage.getScene().getWidth(), stage.getScene().getHeight());
+            stage.setScene(scene);
+            stage.setTitle(bundle.getString("error.undefined"));
+            stage.show();
+            return true;
+        }
+
     }
     
     /**
@@ -64,22 +85,31 @@ public class StageManager {
      * @param owner
      * @param fxml
      */
-    public void addModalStage(Window owner, String fxml) {
-        Scene scene = new Scene(getParent(fxml));
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.initModality(Modality.WINDOW_MODAL);
-        stage.initOwner(owner);
-        stage.setTitle(bundle.getString("error.undefined"));
-        stage.show();
+    public boolean addModalStage(Window owner, String fxml) {
+        if (owner != null) {
+            System.err.println("owner is null!");
+            return false;
+        } else if (fxml != null) {
+            System.err.println("fxml is null!");
+            return false;
+        } else {
+            Scene scene = new Scene(getParent(fxml));
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(owner);
+            stage.setTitle(bundle.getString("error.undefined"));
+            stage.show();
+            return true;
+        }
     }
     
     private Parent getParent(String fxml) {
         lLoader = new FXMLLoader(this.getClass().getResource(fxml), bundle);
         Parent root = null;
         try {
-           return root = lLoader.load();
-        } catch (Exception e) {
+            return root = lLoader.load();
+        } catch (IOException e) {
             System.err.println(e);
             return null;
         }
