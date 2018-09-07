1354da384f87e7bd94734047d062e22dec39ce66
Michał Krzyżanowski <Miisza95@gmail.com>
2018-09-07 20:56:37 +0200

    add logging to stageManager

--- src/main/java/pl/devkrzyzanowski/butler/menager/StageManager.java
+++ src/main/java/pl/devkrzyzanowski/butler/menager/StageManager.java
@@ -31,8 +31,9 @@ public class StageManager {
      *
      */
     public StageManager(Stage stage, String fxml) {
-        this.newStage(stage, fxml);
         setBundle("bundles.messages", Locale.getDefault());
+        newStage(stage, fxml);
+        
     }
     
     public boolean setBundle(String path, Locale locale) {
@@ -62,13 +63,13 @@ public class StageManager {
                 stage.setScene(new Scene(getParent(fxml)));
                 stage.setTitle(bundle.getString("error.undefined"));
                 stage.show();   
-                logger.log(Level.INFO, "Create a new stage from path : {0}", fxml.toString());
+                logger.log(Level.INFO, "Create a new stage from path : {0}", fxml);
                 break;
-            case 1: System.err.println("stage is null!");
+            case 1: logger.log(Level.WARNING, "stage variable is null!");
                 break;
-            case 2: System.err.println("fxml is null!");
+            case 2: logger.log(Level.WARNING, "fxml variable is null!");
                 break;
-            case 3: System.err.println("stage and fxml is null!");
+            case 3: logger.log(Level.WARNING, "stage and fxml variable is null!");
                 break;
             default: 
                 break;
@@ -80,7 +81,8 @@ public class StageManager {
      *
      * @param stage
      * @param fxml
-     * @return true on success, false on fail
+     * @return 0 on succes, 1 if stage is null, 2 if fxml is null, 3 if stage 
+     * and fxml is null
      */
     public Integer changeStage(Stage stage, String fxml) {
         Integer flag = 0;
@@ -94,53 +96,61 @@ public class StageManager {
                 stage.setTitle(bundle.getString("error.undefined"));
                 stage.show();   
                 break;
-            case 1: System.err.println("stage is null!");
+            case 1: logger.log(Level.WARNING, "stage variable is null!");
                 break;
-            case 2: System.err.println("fxml is null!");
+            case 2: logger.log(Level.WARNING, "fxml variable is null!");
                 break;
-            case 3: System.err.println("stage and fxml is null!");
+            case 3: logger.log(Level.WARNING, "stage and fxml variable is null!");
                 break;
             default: 
                 break;
         }
         return flag;        
-
     }
     
     /**
      *
      * @param owner
      * @param fxml
+     * @return 
      */
-    public boolean addModalStage(Window owner, String fxml) {
-        if (owner != null) {
-            System.err.println("owner is null!");
-            return false;
-        } else if (fxml != null) {
-            System.err.println("fxml is null!");
-            return false;
-        } else {
-            Scene scene = new Scene(getParent(fxml));
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.initModality(Modality.WINDOW_MODAL);
-            stage.initOwner(owner);
-            stage.setTitle(bundle.getString("error.undefined"));
-            stage.show();
-            return true;
+    public Integer addModalStage(Window owner, String fxml) {
+        Integer flag = 0;
+        if (owner == null) flag += 1;
+        if (fxml == null || "".equals(fxml)) flag += 2;
+
+        switch (flag) {
+            case 0:
+                Scene scene = new Scene(getParent(fxml));
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.initModality(Modality.WINDOW_MODAL);
+                stage.initOwner(owner);
+                stage.setTitle(bundle.getString("error.undefined"));
+                stage.show();   
+                break;
+            case 1: logger.log(Level.WARNING, "owner variable is null!");
+                break;
+            case 2: logger.log(Level.WARNING, "fxml variable is null!");
+                break;
+            case 3: logger.log(Level.WARNING, "owner and fxml variable is null!");
+                break;
+            default: 
+                break;
         }
+        return flag;            
     }
     
     private Parent getParent(String fxml) {
-        lLoader = new FXMLLoader(this.getClass().getResource(fxml), bundle);
-        Parent root = null;
+        lLoader = new FXMLLoader(getClass().getResource(fxml), bundle);
+            Parent root;
         try {
-            return root = lLoader.load();
+            root = lLoader.load();
         } catch (IOException e) {
-            System.err.println(e);
-            return null;
+            System.err.println("error creating parent root! " + e);
+            root = null;
         }
-        
+        return root;
     }
     
 }
